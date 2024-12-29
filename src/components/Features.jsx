f44d2a4bee360b18e5d65dee8064209f569a7aaa
= <=>
2024-12-29 18:10:28 +0700

    feat: feature, add first feature with implementing Bento Card

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,4 +1,26 @@
-import React from 'react';
+// Private Components, because just this file need
+// to know about them or use them
+const BentoCard = ({ src, title, description, isComingSoon }) => {
+  return (
+    <div className="relative size-full">
+      <video
+        src={src}
+        loop
+        muted
+        autoPlay
+        className="absolute left-0 top-0 size-full object-cover object-center"
+      />
+      <div className="relative z-10 flex size-full flex-col justify-between p-5 text-blue-50">
+        <div>
+          <h1 className="bento-title special-font">{title}</h1>
+          {description && (
+            <p className="mt-3 max-w-64 text-xs md:text-base">{description}</p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const Features = () => {
   return (
@@ -8,16 +30,26 @@ const Features = () => {
           <p className="font-circular-web text-lg text-blue-50">
             Into the Metagame Layer
           </p>
+          <p className="max-w-md font-circular-web text-lg text-blue-50 opacity-50">
+            Immerse yourself in a rich and ever-expanding universe where a
+            vibrant array of products coverge into an interconnected overlay
+            experience on your world
+          </p>
         </div>
-        <p className="max-w-md font-circular-web text-lg text-blue-50 opacity-50">
-          Immerse yourself in a rich and ever-expanding universe where a vibrant
-          array of products coverge into an interconnected overlay experience on
-          your world
-        </p>
-      </div>
 
-      <div className="border-hsla relative mb-7 w-full overflow-hidden rounded-md md:h-[65vh]">
-        {/* Implement Bento Card for every different content */}
+        <div className="border-hsla relative mb-7 w-full overflow-hidden rounded-md md:h-[65vh]">
+          {/* Implement Bento Card for every different content */}
+          <BentoCard
+            src="/videos/feature-1.mp4"
+            title={
+              <>
+                radi<b>n</b>t
+              </>
+            }
+            description="A cross-platform metagame app, turning your activities across Web2 and Web3 games into a rewarding adventure."
+            isComingSoon={false}
+          />
+        </div>
       </div>
     </section>
   );
