7c0359de242cac74777165ec0671ed492af0b1ed
= <=>
2024-12-31 15:46:06 +0700

    feat: story

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -2,6 +2,7 @@ import { useRef } from 'react';
 import AnimatedTitle from './AnimatedTitle';
 import gsap from 'gsap';
 import RoundedCorners from './RoundedCorners';
+import Button from './Button';
 
 const Story = () => {
   const frameRef = useRef('null');
@@ -72,6 +73,21 @@ const Story = () => {
               </div>
             </div>
             <RoundedCorners />
+          </div>
+        </div>
+
+        <div className="-mt-80 flex w-full justify-center md:-mt-64 md:me-44 md:justify-end">
+          <div className="flex h-full w-fit flex-col items-center md:items-start">
+            <p className="mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start">
+              Where realms converge, lies Zentry and the boundless pillar.
+              Discover its secrets and shape your fate amidst infinite
+              opportunities.
+            </p>
+            <Button
+              id="realm-button"
+              title="Discover prologue"
+              containerClass="mt-5"
+            />
           </div>
         </div>
       </div>
